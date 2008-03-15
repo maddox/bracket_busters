c7b1efc833e6fdfa4bcdad61f8f280d5a7d73a48
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-15 01:16:00 -0400

    fixed include and removed activerecord for now

--- bbuster.rb
+++ bbuster.rb
@@ -1,21 +1,26 @@
 $:.unshift File.dirname(__FILE__) + '/lib'
+$:.unshift File.dirname(__FILE__) + '/lib/cbs_sports'
 require 'rubygems'
 require 'sinatra'
-require 'activerecord'
+# require 'active_record'
 require 'cbs_scores'
 require 'team'
 
-ActiveRecord::Base.establish_connection( 
-  :adapter => "mysql", 
-  :database => "bbuster",
-  :username => 'root',
-  :password => '',
-  :host => '127.0.0.1'
-) 
+# ActiveRecord::Base.establish_connection( 
+#   :adapter => "mysql", 
+#   :database => "bbuster",
+#   :username => 'root',
+#   :password => '',
+#   :host => '127.0.0.1'
+# ) 
 
 
 get "/" do
-  @scores = CbsScores.new(:mens_basketball)
-  erb :index
+
+
+# @team = Team.find(:all).each { |team| puts team }
+@scores = CbsScores.new(:mens_basketball)
+
+erb :index
 end
 
