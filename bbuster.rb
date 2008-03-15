02f4db3b1c252460733fdbf529f4b5acc672582c
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-15 03:20:25 -0400

    made pretty

--- bbuster.rb
+++ bbuster.rb
@@ -2,19 +2,8 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/lib/cbs_sports'
 require 'rubygems'
 require 'sinatra'
-# require 'active_record'
 require 'cbs_scores'
-# require 'team'
 
-# ActiveRecord::Base.establish_connection( 
-#   :adapter => "mysql", 
-#   :database => "bbuster",
-#   :username => 'root',
-#   :password => '',
-#   :host => '127.0.0.1'
-# ) 
-
-layout 'default.erb'
 static '/iui', 'iui'
 
 Sinatra::StaticEvent::MIME_TYPES.merge!({'js' => 'application/x-javascript'}) 
@@ -22,7 +11,6 @@ Sinatra::StaticEvent::MIME_TYPES.merge!({'js' => 'application/x-javascript'})
 
 
 get "/" do
-  # @team = Team.find(:all).each { |team| puts team }
   @scores = CbsScores.new(:mens_basketball)
 
   erb :index, :layout => 'default.erb'
